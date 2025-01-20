35a2e96fb8287b57e278c0b5dda72ee57f6ed019
kirillsz <shakhov2013@list.ru>
2025-01-20 19:55:26 +0300

    Title changed

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
     const options = {
       root: null,
       rootMargin: "0px",
-      threshold: 0.1,
+      threshold: 0,
     };
     const observer = new IntersectionObserver(handleObserver, options);
     if (catLoader.current) {
